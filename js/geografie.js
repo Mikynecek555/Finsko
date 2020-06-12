532d7c8d6f2328adea51ad4010331478feb8b665
unknown <mikynecek555@gmail.com>
2020-06-12 15:57:28 +0200

    “Update5”

---
+++ js/geografie.js
@@ -0,0 +1,217 @@
+let mesta = [
+    {
+        "id": "rovaniemi",
+        "city": "Rovaniemi",
+        "people": "61784",
+        "sign": "Rovaniemi.jpg",
+        "text": "Rovaniemi (Roavenjárga v severní sámštině a Ruä'vnjargg v skoltské sámštině) je město na severu Finska. Je administrativním a obchodním centrem finské nejsevernější provincie – Laponska. Centrum leží asi pět km jižně od severního polárního kruhu mezi vrchy Ounas a Korkalo na soutoku řek Kemi a Ounas. Na začátku roku 2006 se město sloučilo se svým vesnickým okolím do jediné obce, čímž se město stalo jedním z rozlohou největších měst na světě a největším v Evropě, ačkoli v něm bydlí jen asi 60 000 lidí."
+    },
+    {
+        "id": "oulu",
+        "city": "Oulu",
+        "people": "200526",
+        "sign": "Oulu.jpg",
+        "text": "Oulu (švédsky Uleåborg) je největší město v severním Finsku. Leží na severovýchodním pobřeží Botnického zálivu Baltského moře při ústí řeky Oulu v provincii Severní Pohjanmaa. Se svými více než 200 000 obyvatel se po Turku a před Jyväskylä řadí mezi největší finská města."
+    },
+    {
+        "id": "kajaani",
+        "city": "Kajaani",
+        "people": "37622",
+        "sign": "Kajaani.jpg",
+        "text": "Kajaani (švédsky Kajana) je město na severu Finska v provincii Kainuu, jejíž je správním střediskem. Leží na břehu jezera Oulujärvi. Kajaani leží v subarktickém podnebném pásmu, zimní teploty tu klesají až k -30 °C.Město vzniklo v polovině 17. století. Stalo se významným producentem dehtu vyráběného z borovicového dřeva, který byl dopravován po řece Oulujoki na pobřeží Botnického zálivu do města Oulu a následně do celé Evropy. Dehet se využíval při stavbě lodí. Těžba dřeva a na ní navázaný dřevozpracující průmysl dodnes hrají významnou roli v ekonomice města i celého regionu."
+    },
+    {
+        "id": "kokkola",
+        "city": "Kokkola",
+        "people": "47570",
+        "sign": "Kokkola.jpg",
+        "text": "Kokkola (švédsky Karleby) je město na severozápadě Finska v provincii Střední Pohjanmaa, jejíž je správním střediskem. Nachází se na pobřeží Botnického zálivu."
+    },
+    {
+        "id": "vaasa",
+        "city": "Vaasa",
+        "people": "67619",
+        "sign": "Vaasa.jpg",
+        "text": "Vaasa [ˈvɑːsɑ] (švédsky Vasa) je západofinské přístavní město ležící na břehu Botnického zálivu. S městem Umeå je spojuje trajekt, jenž je nejkratším námořním spojením mezi Finskem a Švédskem. Letiště Vaasa se nachází 9 kilometrů jihovýchodně od centra města."
+    },
+    {
+        "id": "seinajoki",
+        "city": "Seinäjoki",
+        "people": "61530",
+        "sign": "seinajoki.jpg",
+        "text": "Seinäjoki [ˈsɛi̯næjɔki] je město na západě Finska. Je správním centrem provincie Jižní Pohjanmaa a významným železničním uzlem. Železniční trati spojují Seinäjoki s městy Tampere, Kaskinen, Vaasa, Oulu a Haapamäki. Letiště Seinäjoki se nachází v sousedním Ilmajoki, 11 kilometrů jižně od centra Seinäjoki."
+    },
+    {
+        "id": "pori",
+        "city": "Pori",
+        "people": "85363",
+        "sign": "pori.jpg",
+        "text": "Pori (švédsky Björneborg) je město a obec, nacházející se na západním pobřeží Finska. Střed města leží asi 15 km od Botnického zálivu u ústí řeky Kokemäenjoki. Pori je nejdůležitějším městem provincie Satakunta. Ve městě žije 83 192 lidí (k 31. lednu 2012) a jeho rozloha činí 511,88 km², z čehož 8,71 km² jsou vodní plochy. Hustota zalidnění je zde 151,4 obyvatele na km². Město je jednojazyčné, finské. Pori je desáté největší město ve Finsku. Neogotické Juseliovo mauzoleum na hřbitově Käppärä ve středu města bylo postaveno v roce 1903 bohatým průmyslníkem F. A. Juseliem na památku jeho dcery, která zemřela v jejích 11 letech. Mauzoleum navrhl architekt Josef Stenbäck. Mauzoleum bylo původně vyzdobené freskami Akseliho Gallena-Kallely, ale ty byly později zničeny. Restauroval je Akseliho syn Jorma Gallen-Kallela s využitím skic svého otce. Pori je kromě jiného známé písečnou pláží Yyteri, každoročním mezinárodním jazzovým festivalem Pori Jazz a výrobou piva Karhu."
+    },
+    {
+        "id": "turku",
+        "city": "Turku",
+        "people": "186014",
+        "sign": "Turku.jpg",
+        "text": "Turku je finské město a přístav v jihozápadním Finsku, administrativní centrum provincie Vlastní Finsko. Město bylo založeno koncem 13. století při ústí řeky Aury do Baltského moře, 150 km západně od Helsinek. Do roku 1812 bylo správním centrem Finska. V roce 2016 zde žilo přes 186 000 obyvatel a bylo tak šestým největším finským městem. V celé městské oblasti žilo přes 315 000 obyvatel. 5,2 % obyvatel mluví švédsky. V Turku jsou loděnice, jejichž součástí je i suchý dok o rozměrech 365×80 m, kde se staví největší osobní lodě světa (např. Oasis of the Seas). Arcibiskup Turku je primasem Evangelicko-luterské církve Finska. V roce 1920 zde byla založena univerzita. Turku bylo roku 2011 Evropským hlavním městem kultury."
+       },
+    {
+        "id": "tampere",
+        "city": "Tampere",
+        "people": "225118",
+        "sign": "tampere.jpg",
+        "text": "Tampere (švédsky Tammerfors) je třetím největším městem Finska a největším vnitrozemským městem v severní Evropě. Bylo založeno roku 1779. Tampere leží v západním Finsku a jeho centrum je mezi jezery Näsi a Pyhä. Patří k němu ale i poměrně rozsáhlá oblast v okolí těchto jezer. Rozkládá se na 523,42 km² a má přibližně 210 tisíc obyvatel."
+    },
+    {
+        "id": "hameenlinna",
+        "city": "Hämeenlinna",
+        "people": "68011",
+        "sign": "ham.jpg",
+        "text": "Hämeenlinna (švédsky Tavastehus, přímým překladem „Hämeský hrad“) je obec a město ve Finsku. Leží v provincii Kanta-Häme. Žije v něm asi 48 000 obyvatel. Leží zhruba na půli cesty mezi Helsinkami (100 km) a Tampere (73 km). Ve městě stojí známý středověký hrad Hämeen linna. Do konce roku 2009 byla sídlem hejtmana kraje Jižní Finsko. Území města bylo obydleno už v době železné. V 11. století byla celá tehdejší provincie pod vlivem jak Švédska, tak ruského Novgorodu. Území bylo ovládáno z místního hradu či pevnosti. V té době zde vznikla i vesnice, která poskytovala zázemí obyvatelům hradu. Hämeenlinna byla povýšena na město v roce 1639 krátce poté, co byla na příkaz švédského krále přesunuta o kilometr jižněji na kopec, kde stojí dodnes. Město je také známo svými školami a akademiemi, kde studovalo mnoho známých Finů. Školy, vláda a armáda určovaly život Hämeenlinny po celou její historii."
+    },
+    {
+        "id": "helsinki",
+        "city": "Helsinki",
+        "people": "650100",
+        "sign": "helsinki.jpg",
+        "text": "Helsinky jsou hlavní a největší finské město a přístav. Byly založeny roku 1550. Jejich centrum leží na malém poloostrovu ve středu Finského zálivu, ale patří k nim ještě poměrně velké území směrem na východ. Rozkládají se na 715,55 km² a mají asi 650 100 obyvatel. Se sousedními městy Vantaa, Espoo a Kauniainen tvoří hustě propojené souměstí (tzv. metropolitní oblast) s miliónovou populací. Spolu s osmi dalšími městy tvoří Velké Helsinky s 1,495 mil. obyvatel, což je přibližně čtvrtina celého Finska; Velké Helsinky produkují třetinu finského HDP."
+    },
+    {
+        "id": "jyvaskyla",
+        "city": "Jyväskylä",
+        "people": "137368",
+        "sign": "jyva.jpg",
+        "text": "Jyväskylä je město v provincii Střední Finsko, 140 km severovýchodně od Tampere a 270 km severně od Helsinek, na severním konci jezera Päijänne. S 129 425 obyvateli (31. 5. 2010) je to sedmé největší město Finska. Jeho rozloha je 136,9 km², z čehož je 31 km² vody. Díky velkému počtu škol je město známo jako „Finské Athény“. Mnoho budov v Jyväskylä bylo navrženo Alvarem Aaltem. Město každoročně hostí motoristickou soutěž Neste Rally Finland, dříve známou pod názvem „Rally tisíce jezer“."
+    },
+    {
+        "id": "lahti",
+        "city": "Lahti",
+        "people": "118119",
+        "sign": "lahti.jpg",
+        "text": "Lahti (švédsky Lahtis) je město v jižním Finsku. Leží na jižním okraji jezera Vesijärvi. Nachází se na 61° severní šířky asi 100 km od Helsinek. Se svými zhruba 100 000 obyvateli se mezi finskými městy řadí na osmé místo mezi Jyväskylä a Kuopio. Město se rozkládá na 154,60 km², z čehož 19,56 km² je vodní plochy. Samostatným městem se stalo v roce 1905. Původně bylo součástí sousedícího města Hollola, ale dnes je již mnohem větší než jeho soused. Se současným počtem obyvatel se řadí po Tampere a Jyväskylä na třetí místo mezi finskými vnitrozemskými městy. Město má železniční spojení s ostatním většími finskými městy. Do Helsinek vede i dálnice. Lahti společně s Kuopiem je hlavním zimním a horským centrem. V obou městech se také koná mistrovství světa ve skocích na lyžích. Lahti se navíc může pyšnit nejstarším skokanským můstkem ve Finsku, který pochází z roku 1880. Co se týče běžeckého lyžování, konalo se zde již šestkrát mistrovství světa (poprvé v roce 1926 a naposledy v roce 2017)."
+    },
+    {
+        "id": "kouvola",
+        "city": "Kouvola",
+        "people": "85855",
+        "sign": "kouvola.jpg",
+        "text": "Kouvola je město na jihu Finska. Je největším (jak rozlohou, tak i obyvatelstvem) městem provincie Kymenlaakso. Městem protéká řeka Kymijoki. Do roku 1743 byla Kouvola součástí Finska spravovaného Švédskem. Po podepsání smlouvy z Turku v roce 1743 přešla pod Ruské impérium. V roce 1812 byla opětovně přičleněna k finskému velkoknížectví."
+    },
+    {
+        "id": "lappeenranta",
+        "city": "Lappeenranta",
+        "people": "72875",
+        "sign": "lap.jpg",
+        "text": "Lappeenranta je obec a město, které leží na břehu jezera Saimaa v jihovýchodním Finsku, asi 30 km od hranice s Ruskem. Patří do provincie Jižní Karélie. Žije v něm 71 811 obyvatel, což ho řadí na 13. místo mezi největšími finskými městy. Na město byla Lappeenranta povýšena roku 1649 královnou Kristýnou, čímž se uzákonil obchod na tehdy oblíbeném trhu Lapvesi. Město je podobně jako mnohá jiná finská města univerzitní, Lappeenrantská technická univerzita patří mezi největší místní zaměstnavatele. Blízkost ruské hranice je znatelná hlavně tím, že do města přijíždí stále více ruských turistů. Sankt Petěrburg (211 km) je městu blíže než Helsinky (221 km)."
+    },
+    {
+        "id": "mikkeli",
+        "city": "Mikkeli",
+        "people": "54665",
+        "sign": "mikkeli.jpg",
+        "text": "Mikkeli je finské město v provincii Jižní Savo na břehu jezera Saimaa. Do roku 2010 bylo hlavním městem kraje Východní Finsko. Má 46 612 obyvatel a náleží mu území o rozloze 1622,11 km² (z toho 303,57 km² tvoří vodní plochy). V čase druhé světové války zde sídlil štáb finského národního hrdiny a vrchního velitele finských vojsk, maršála Mannerheima. Je zde několik válečných muzeí včetně muzea vrchního velení."
+    },
+    {
+        "id": "joensuu",
+        "city": "Joensuu",
+        "people": "75514",
+        "sign": "joen.jpg",
+        "text": "Joensuu (v překladu „Ústí řeky“) je hlavní město provincie Severní Karélie ležící ve východním Finsku. Město bylo založeno v roce 1848 ruským carem Mikulášem I. a v současnosti v něm žije přibližně 74 000 obyvatel. Město je charakteristické přírůstkem obyvatelstva. Na této charakteristice se podílí zejména administrativní změny. V roce 2005 se Joensuu sloučila se sousedními obcemi Kiihtelysvaara a Tuupovaara, v roce 2009 dále s obcemi Eno a Pyhäselkä. Vzdálenost od Helsinek je přibližně 440 km, od ruských hranic přibližně 80 km. Ve městě se nachází kampus Východofinské univerzity (finsky Itä-Suomen yliopisto), která vznikla sloučením Joensuuské a Kuopioské univerzity v roce 2010. A také Karelská univerzita aplikovaných věd. Také díky přítomnosti těchto univerzit je Joensuu populačně velmi mladé město, téměř 20 000 obyvatel jsou žáci a studenti."
+    },
+    {
+        "id": "kuopio",
+        "city": "Kuopio",
+        "people": "110999",
+        "sign": "kuo.jpg",
+        "text": "Kuopio je město ve východním Finsku, 330 km severovýchodně od Helsinek. Je administrativním centrem provincie Severní Savo. V roce 2015 zde žilo přes 110 000 obyvatel a bylo tak devátým největším finským městem. Jeho centrum leží na poloostrově devátého největšího finského jezera jezera Kallavesi a z celkové rozlohy 3 164 km² zaujímá 605 km² vodní plocha. Nejvyšším bodem města je kopec Puijo s výškou 232 m n. m."
+    }
+];
+
+let unesco = [
+    {
+        "id": "rauma",
+        "nazev": "Vanha Rauma",
+        "vznik": "17. století",
+        "unes": 1991,
+        "fotka": "rauma.jpg",
+        "popis": "Historické centrum města Rauma ležícího na západním pobřeží Finska. Jde o unikátní zachovaný příklad dřevěné městské architektury. Nachází se zde několik set dřevěných domů, některé již ze sedmnáctého století, většina domů pak ze století osmnáctého.",
+        "url": "http://whc.unesco.org/en/list/582"
+    },
+    {
+        "id": "pohreb",
+        "nazev": "Pohřebiště z doby bronzové Sammallahdenmäki",
+        "vznik": "3 000 let př. n. l.",
+        "unes": 1996,
+        "fotka": "sammall.jpg",
+        "popis": "Sammallahdenmäki je pohřebiště z doby bronzové, jehož stáří se odhaduje až na tři tisíce let. pohřebiště se nachází nedaleko finského městečka Lappi v provincii Satakunta na jihozápadě Finska.",
+        "url": "http://whc.unesco.org/en/list/579"
+    },
+    {
+        "id": "kostel",
+        "nazev": "Kostel v Petäjävesi",
+        "vznik": "18. století",
+        "unes": 1994,
+        "fotka": "peta.jpg",
+        "popis": "Kostel v Petäjävesi byl postaven ve finské vesnici Petäjävesi mezi lety 1763-1765. Je celý ze dřeva a je ukázkou skandinávské architektury, avšak zároveň je postaven pod vlivem renesance a nese i gotické prvky. ",
+        "url": "http://whc.unesco.org/en/list/584"
+    },
+    {
+        "id": "verla",
+        "nazev": "Verla",
+        "vznik": "19. století",
+        "unes": 1996,
+        "fotka": "verla.jpg",
+        "popis": "Verla je dobře zachovalá malá vesnice ve finském městě Kouvola. Je známá především díky svým továrnám na výrobu papíru, které zde stojí dodnes a jsou unikátní zejména technologickou ukázkou. První továrny zde byly zakládány již ve 2. polovině 19. století. Dnes je Verla často navštěvovanou turistickou lokalitou.",
+        "url": "http://whc.unesco.org/en/list/751"
+    }
+    ];
+    $(function(){
+        $("#mapa ellipse").hide();
+        $("#mapa rect").hide();
+        $("#mesta").on('change', function(){
+            $("#mapa ellipse").toggle(500);
+        });
+        $("#unesco").on('change', function(){
+            $("#mapa rect").toggle(500);
+        });
+        let puvodni = '';
+        $("#mapa path").on('mouseover', function(){
+            if ($("#provincie:checked").val()) {
+                puvodni = $(this).css('fill');
+                $(this).css('fill','orange');
+                $(this).popover({
+                    'trigger':'click',
+                    'placement': 'right',
+                    'offset': '10%, -100',                 
+                    'title': $(this).find('title').text()
+    
+                });            
+            }
+        });
+
+
+        $("#mapa path").on('mouseout', function(){
+            $(this).css('fill', puvodni);
+        });
+
+
+        $("#mapa ellipse").on('click', function(){
+            let id = $(this).attr('id');
+            $("#mapa ellipse").css('fill','red');
+            $("#mapa rect").css('fill','green');
+            $(this).css('fill','blue');
+            let mesto = mesta.find(item => {return item.id == id});
+            $('#info').fadeOut(1000, function() {$('#info').html(`<div class="col-12"><h2 id="mesta" class="bg-primary"> ${mesto.city} <small class="text-small">${new Intl.NumberFormat('cs-CS').format(mesto.people)} obyvatel</small></h2></div><div class="col-2"><img src="img/${mesto.sign}" class="img-fluid"></div><div class="col-10">${mesto.text}</div>`)});
+            $('#info').fadeIn(1000);
+        });
+
+        $("#mapa rect").on('click', function(){
+            let id = $(this).attr('id');
+            $("#mapa ellipse").css('fill','red');
+            $("#mapa rect").css('fill','green');
+            $(this).css('fill','purple');
+            let pamatka = unesco.find(item => {return item.id == id});
+            $('#info').slideUp(1000, function(){$('#info').html(`<div class="col-12"><h2 id="mesta" class="bg-info">${pamatka.nazev} <small class="text-small">(Tato památka byla vytvořena v ${pamatka.vznik} a v seznamu unesco je zapsána od roku ${pamatka.unes})</small></h2></div><div class="col-4"><img src="img/${pamatka.fotka}" class="img-fluid"></div><div class="col-8"><p>${pamatka.popis}</p><p><a href="${pamatka.url}" target="_blank">Více informací: "${pamatka.url}"</a></p></div>`)});
+            $('#info').slideDown(1000);
+        });  
+});
